e6df31e6629044d31dd158baf4ff172a0f04583d
AhmedElhlawany <ahmedelhalawany429@gmail.com>
2025-08-12 15:05:02 +0300

    Update places.jsx

--- src/components/places/places.jsx
+++ src/components/places/places.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Style from './Places.module.css';
+import Style from './places.module.css';
 import { useNavigate } from 'react-router-dom';
 
 export default function Places({ city }) {
